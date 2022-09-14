f03b494c75a3d22447546460cb4e5edf5de76e94
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-09-14 17:27:04 +0900

    Update project.py

--- project.py
+++ project.py
@@ -38,7 +38,7 @@ st.sidebar.write('keyword is', keyword)
 
 platform = ['naver_news','naver_cafe','naver_blog', 'daum_news','daum_cafe','daum_blog','youtube','tweeter','facebook','instagram']
 platform_selections = st.sidebar.multiselect(
-    "Select Accounts to View", options=platform, default=platform
+    "Select Platforms to View", options=platform, default=platform
 )
 
 
@@ -51,7 +51,7 @@ End_date = st.sidebar.date_input(
 st.sidebar.write('End date is:', End_date)
 
 ##############################################################
-head="키워드 " +keyword+"에 대한 검색결과는 다음과 같습니다."
+head="키워드 " +keyword+"에 대한 플랫폼별 검색결과는 다음과 같습니다."
 st.header(head)
 
 df=pd.read_csv("example2.csv", encoding='utf8')
@@ -59,8 +59,5 @@ st.dataframe(df)
 
 for platform in platform_selections:
     expander = st.expander(platform)
-    expander.write("""
-         The chart above shows some numbers I picked for you.
-         I rolled actual dice for these, so they're *guaranteed* to
-         be random.
-     """)
+    dataframe=df[df['platform']==platform]
+    expander.write(dataframe)
